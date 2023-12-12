d9cdf8027b7e6bf359ae6efc042d67a6725d8a12
Ahmet Can <acanyilmaz2022@gmail.com>
2023-12-12 16:01:07 +0300

    add project public template

---
+++ 202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = ""
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "vxyz"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us"
+}
